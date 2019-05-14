c8839814d9ecb8d7e211316e7f47612014f03297
Jonathan George <jwesleygeorge@gmail.com>
2019-05-14 12:51:29 -0500

    Updated tests

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,4 +1,5 @@
 const chai = require("chai")
+    , should = chai.should()
     , chaiHttp = require("chai-http");
 const mocha = require('mocha');
 const server = require("../app");
@@ -8,5 +9,27 @@ chai.use(chaiHttp);
 describe("test", () => {
     it("should return status 200", () => {
         chai.request(server).get('/')
+    });
+});
+
+describe("POST /auth/register", () => {
+    it("should register a new user", (done) => {
+        chai.request(server)
+        .post("/auth/register")
+        .send({
+            email: "example@example.com",
+            password: "password",
+            username: "test",
+            first_name: "First",
+            last_name: "Last"
+        })
+        .end((err, res) => {
+            should.not.exist(err);
+            res.redirects.length.should.eql(0);
+            res.status.should.eql(200);
+            res.type.should.eql("application/json");
+            res.body.statys.should.eql("success");
+            done();
+        })
     })
 })
